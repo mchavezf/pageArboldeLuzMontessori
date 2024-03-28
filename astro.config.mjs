b629a2502837bbf851c223636ce5619da154571c
matias <matchavezf@gmail.com>
2024-03-28 11:32:49 -0300

    v0.02 corrige site base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://mchavezf.github.io',
-  base: 'my-pageArboldeLuzMontessori',
+  base: 'pageArboldeLuzMontessori',
 });
